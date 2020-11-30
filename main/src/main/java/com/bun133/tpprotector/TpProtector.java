8a9dad61b97acfa66096ab4c70b30416d6871464
Bun133 <bun133.work@gmail.com>
2020-12-01 08:00:34 +0900

    Ending up!

--- main/src/main/java/com/bun133/tpprotector/TpProtector.java
+++ main/src/main/java/com/bun133/tpprotector/TpProtector.java
@@ -12,6 +12,8 @@ public final class TpProtector extends JavaPlugin {
         this.protector = new Protector();
         this.getServer().getPluginManager().registerEvents(protector,this);
         this.command = new TPCommand(protector);
+        this.getCommand("t").setExecutor(command);
+        this.getServer().getScheduler().runTaskTimer(this,protector,1,1);
     }
 
     @Override
--- main/src/main/kotlin/com/bun133/tpprotector/Protector.kt
+++ main/src/main/kotlin/com/bun133/tpprotector/Protector.kt
@@ -1,49 +1,58 @@
 package com.bun133.tpprotector
 
+import org.bukkit.Bukkit
 import org.bukkit.entity.Entity
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.scheduler.BukkitRunnable
 
-class Protector : Listener{
-    var list:MutableMap<Entity,Int> = hashMapOf()
+class Protector : Listener, BukkitRunnable() {
+    var list: MutableMap<Entity, Int> = hashMapOf()
 
-    fun set(p:Entity,ticks:Int){
-        if(list.contains(p)){
-            if(list[p]!! < ticks){
+    fun set(p: Entity, ticks: Int) {
+        if (list.contains(p)) {
+            if (list[p]!! < ticks) {
                 list[p] = ticks
             }
-        }else list[p] = ticks
+        } else list[p] = ticks
     }
 
-    fun setAll(p:MutableList<Entity>,ticks:Int) {
-        p.forEach { set(it,ticks) }
+    fun setAll(p: MutableList<Entity>, ticks: Int) {
+        p.forEach { set(it, ticks) }
     }
 
-    private fun isRemaining(p:Entity):Boolean{
-        if(list.containsKey(p)){
+    private fun isRemaining(p: Entity): Boolean {
+        if (list.containsKey(p)) {
             return list[p]!! > 0
         }
         return false
     }
 
-    fun shrink(ticks:Int){
-        for(key in list.keys){
-            list[key] = list[key]!! - ticks
+    fun shrink(ticks: Int) {
+        for (key in list.keys) {
+            if (list[key] != 0) {
+                list[key] = list[key]!! - ticks
+            }
         }
     }
 
     @Override
     @EventHandler
-    fun onDamage(e:EntityDamageEvent){
-        if(e.cause === EntityDamageEvent.DamageCause.CRAMMING){
-            if(isRemaining(e.entity)){
+    fun onDamage(e: EntityDamageEvent) {
+        if (e.cause === EntityDamageEvent.DamageCause.CRAMMING) {
+            if (isRemaining(e.entity)) {
                 e.isCancelled = true
-                if(e.entity is Player){
+                if (e.entity is Player) {
                     e.entity.sendMessage("You Protected From Cramming Damage!")
                 }
+//                Bukkit.broadcastMessage("You Protected From Cramming Damage!")
             }
         }
+    }
+
+    override fun run() {
+        shrink(1)
     }
 }
