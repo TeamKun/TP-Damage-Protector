9956e2ca79ba50fdfc3e1393dfc28370e7306e55
Bun133 <bun133.work@gmail.com>
2020-12-01 08:19:59 +0900

    Real Real!! Ending up!

--- main/src/main/kotlin/com/bun133/tpprotector/commands/TPCommand.kt
+++ main/src/main/kotlin/com/bun133/tpprotector/commands/TPCommand.kt
@@ -1,7 +1,9 @@
 package com.bun133.tpprotector.commands
 
 import com.bun133.tpprotector.Protector
+import com.sun.org.apache.xpath.internal.operations.Bool
 import org.bukkit.Bukkit
+import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -18,6 +20,11 @@ class TPCommand (var protector: Protector): CommandExecutor {
                     if(Bukkit.selectEntities(sender,args[0]).isNotEmpty()){
                         return run(Bukkit.selectEntities(sender,args[0])[0] as Player,Bukkit.selectEntities(sender,args[1]))
                     }else return false
+                }else if(args.size==4){
+                    if(args[1].toDoubleOrNull() != null && args[2].toDoubleOrNull() != null && args[3].toDoubleOrNull() != null){
+                        val loc:Location = Location(sender.location.world,args[1].toDouble(),args[2].toDouble(),args[3].toDouble())
+                        return run(loc,Bukkit.selectEntities(sender,args[0]))
+                    }
                 }else return false
             }
         }
@@ -26,6 +33,12 @@ class TPCommand (var protector: Protector): CommandExecutor {
 
     private fun run(player:Player, players:MutableList<Entity>):Boolean{
         players.forEach {it.teleport(player) }
+        protector.setAll(players,20)
+        return true
+    }
+
+    private fun run(loc:Location,players: MutableList<Entity>):Boolean{
+        players.forEach {it.teleport(loc) }
         protector.setAll(players,20)
         return true
     }
